e4aafc735f491ed18e514d5983f70dddf60e7347
Jisie DM <jisie.david@gmail.com>
2020-11-24 07:43:29 -0800

    Added routing with Navbar and ApplicationViews

--- src/components/Kennel.js
+++ src/components/Kennel.js
@@ -2,6 +2,7 @@ import React from "react"
 import { Animal } from "./animal/Animal"
 import { AnimalList } from "./animal/AnimalList"
 import { AnimalProvider } from "./animal/AnimalProvider"
+import { ApplicationViews } from "./ApplicationViews"
 import { Customer } from "./customer/Customer"
 import { CustomerList } from "./customer/CustomerList"
 import { CustomerProvider } from "./customer/CustomerProvider"
@@ -12,34 +13,11 @@ import "./Kennel.css"
 import { Location } from "./location/Location"
 import { LocationList } from "./location/LocationList"
 import { LocationProvider } from "./location/LocationProvider"
+import { NavBar } from "./nav/NavBar"
 
 export const Kennel = () => (
   <>
-    <h2>Nashville Kennels</h2>
-    <small>Loving care when you're not there.</small>
-    <address>
-      <div>Visit Us at the Nashville North Location</div>
-      <div>500 Puppy Way</div>
-    </address>
-
-    <h2>Animals</h2>
-    <AnimalProvider>
-      <AnimalList />
-    </AnimalProvider>
-
-    <h2>Employees</h2>
-    <EmployeeProvider>
-      <EmployeeList />
-    </EmployeeProvider>
-
-    <h2>Locations</h2>
-    <LocationProvider>
-      <LocationList />
-    </LocationProvider>
-
-    <h2>Customers</h2>
-    <CustomerProvider>
-      <CustomerList />
-    </CustomerProvider>
+    <NavBar />
+    <ApplicationViews />
   </>
 )
