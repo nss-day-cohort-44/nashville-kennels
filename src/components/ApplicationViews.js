3fa04859d87c0f8aa71e185a5cd58a79a774854e
Jisie DM <jisie.david@gmail.com>
2020-12-01 13:26:45 -0800

    Location details

--- src/components/ApplicationViews.js
+++ src/components/ApplicationViews.js
@@ -9,16 +9,27 @@ import { EmployeeList } from "./employee/EmployeeList"
 import { EmployeeProvider } from "./employee/EmployeeProvider"
 import { EmployeeForm } from "./employee/EmployeeForm"
 import { EmployeeDetail } from "./employee/EmployeeDetail"
+import { LocationDetail } from "./location/LocationDetail"
 
 export const ApplicationViews = (props) => {
   return (
     <>
-      <LocationProvider>
-        {/* Render the location list when http://localhost:3000/ */}
-        <Route exact path="/">
-          <LocationList />
-        </Route>
-      </LocationProvider>
+      <AnimalProvider>
+        <EmployeeProvider>
+          <LocationProvider>
+            {/* Render the location list when http://localhost:3000/ */}
+            <Route exact path="/">
+              <LocationList />
+            </Route>
+            <Route path="/locations/:locationId(\d+)" render={
+              props => {
+                console.log("props", props)
+              return <LocationDetail {...props} />
+            }
+            } />
+          </LocationProvider>
+        </EmployeeProvider>
+      </AnimalProvider>
 
       <AnimalProvider>
         <LocationProvider>
